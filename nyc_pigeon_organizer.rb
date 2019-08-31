4fcd49088b519a3417c43869dbb026c6da3fc82e
ivillicana <31767556+ivillicana@users.noreply.github.com>
2019-08-31 15:00:45 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  result = {}
+  data.each do |attribute, attr_hash|
+    attr_hash.each do |nested_attr, names_array| 
+      names_array.each do |name|
+        result[name] = {} if !result[name]
+        result[name][attribute] = [] if !result[name][attribute]
+        result[name][attribute] << nested_attr.to_s if !result[name][attribute].include?(nested_attr)
+      end
+    end
+  end
+  result
 end
